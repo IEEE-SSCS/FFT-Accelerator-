984a0c735bf89af42432b03b73a107ba3f078679
youssef mehdawy <mohamedmehdawy@yahoo.com>
2022-05-16 21:45:25 +0200

    Add cordic ports.

---
+++ rtl/Cordic.sv
@@ -0,0 +1,20 @@
+module cordic #(
+parameter data_width=8
+)
+(
+input logic clk,nrst,
+input  logic [data_width-1:0] in_real,in_img,
+input  logic [data_width-1:0] FA_sgn,
+output logic [data_width-1:0] out_real,out_img
+);
+
+
+
+
+
+
+
+
+
+
+endmodule
